9646da41bd0627748e007ef26d35295df70ad147
mycodeclub <ankitsahay1990@gmail.com>
2018-07-10 10:13:53 +0530

    Removed space validation from Aadhar too
    Added Unique filter for Aadhar and pan in BusinessPartnerProgramme

--- MeriMudra/Models/MmDbContext.cs
+++ MeriMudra/Models/MmDbContext.cs
@@ -4,7 +4,10 @@ namespace MeriMudra.Models
 {
     public class MmDbContext : DbContext
     {
-        public MmDbContext() : base("name=MmDbConnectionString") { }
+        public MmDbContext() : base("name=MmDbConnectionString")
+        {
+            Database.SetInitializer<MmDbContext>(null); //disable initializer i.e. disable code first migrations  
+        }
         public virtual DbSet<City> Citys { get; set; }
         public virtual DbSet<State> States { get; set; }
         public virtual DbSet<BusinessPartnerProgramme> BusinessPartnerProgrammes { get; set; }
