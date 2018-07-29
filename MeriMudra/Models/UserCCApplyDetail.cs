535b0c30f3cbdb9ac406c663a30e4cf145f0b5fd
nirvesh <nirvesh00@gmail.com>
2018-07-29 11:19:39 +0530

    complete  Loan apply view for user

--- MeriMudra/Models/UserCCApplyDetail.cs
+++ MeriMudra/Models/UserCCApplyDetail.cs
@@ -61,7 +61,65 @@ namespace MeriMudra.Models
         }
 
     }
+    [Table("UserCCApplyDetail")]
+    public class UserLoanApplyDetail
+    {
+        [Key]
+        public int Id { get; set; }
 
+        public bool? EmployerType { get; set; }
+        [NotMapped]
+        public string employed_radios { get; set; }
+        public string CompanyName { get; set; }
+        public decimal? GrossIncomeOrNetSalary { get; set; }
+        public string Name { get; set; }
+        public DateTime? DOB { get; set; }
+        public string CityName { get; set; }
+        [NotMapped]
+        public string CityId { get; set; }
+        [NotMapped]
+        public string PinCode { get; set; }
+        public string MobileNumber { get; set; }
+        public string email { get; set; }
+        public List<string> AccountWith { get; set; }
+        public List<string> CreditCardWith { get; set; }
+        public decimal? CreditCardMaxLimit { get; set; }
+        [NotMapped]
+        public string CurrentOrPrevLoan { get; set; }
+        [NotMapped]
+        public bool? _CurrentOrPrevLoan { get; set; }
+        public int OTP { get; set; }
+        public bool isMobileNumberVerify { get; set; }
+        public bool isEmailVerify { get; set; }
+        public bool isUserActive { get; set; }
+        public decimal? Intended_loan_amount { get; set; }
+        public UserLoanApplyDetail()
+        {
+            Intended_loan_amount = 0;
+            Id = 0;
+            EmployerType = true;
+            employed_radios = "";
+            CompanyName = "";
+            GrossIncomeOrNetSalary = 0;
+            Name = "";
+            DOB = DateTime.Now;
+            CityName = "";
+            CityId = "";
+            PinCode = "";
+            MobileNumber = "";
+            email = "";
+            AccountWith = new List<string>();
+            CreditCardWith = new List<string>();
+            CreditCardMaxLimit = 0;
+            CurrentOrPrevLoan = "";
+            _CurrentOrPrevLoan = false;
+            OTP = 0;
+            isMobileNumberVerify = false;
+            isEmailVerify = false;
+            isUserActive = false;
+        }
+
+    }
     public class detailsForApplyCard
     {
         public List<Bank> Banks { get; set; }
