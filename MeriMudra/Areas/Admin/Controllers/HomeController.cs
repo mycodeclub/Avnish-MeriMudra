25c1df9581906a4458d2c2dacdc65125e73d9fb4
nirvesh <nirvesh00@gmail.com>
2018-07-31 14:45:27 +0530

    Conflict resolve

--- MeriMudra/Areas/Admin/Controllers/HomeController.cs
+++ MeriMudra/Areas/Admin/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace MeriMudra.Areas.Admin.Controllers
         public ActionResult Dashboard()
         {
             var data = db.UserCCApplyDetail.ToList();
+            ViewData["loan_data"] = db.userLoanApplyDetail.ToList();
             return View(data);
         }
     }
