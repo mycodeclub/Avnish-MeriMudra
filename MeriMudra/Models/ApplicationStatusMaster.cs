41e1df345dd1f534deb92154d111de9f476915b9
mycodeclub <ankitsahay1990@gmail.com>
2018-09-03 22:21:47 +0530

    Added ApplicationStatusMaster

--- MeriMudra/Models/ApplicationStatusMaster.cs
+++ MeriMudra/Models/ApplicationStatusMaster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace MeriMudra.Models
     [Table("ApplicationStatusMaster")]
     public class ApplicationStatus
     {
+        [Key]
         public int StatusId { get; set; }
         public string Status { get; set; }
     }
